a0a374bc87c92f2661d55a9237b715a7f1c1dc0f
srija <srijan1214@gmail.com>
2019-11-25 21:45:39 -0500

    Added functionality to go fly to location when user clicks on the side bar.

--- scripts/project_2.js
+++ scripts/project_2.js
@@ -1,54 +1,3 @@
-
-//Utility function to binary search the first array cloumn of a 2d array sorted according to the first column.
-//O(Log(n))
-function BinarySearch2dArray(x, arr) {
-	let start = 0, end = arr.length - 1;
-	// Iterate while start not meets end
-	while (start <= end) {
-		// Find the mid index
-		let mid = Math.floor((start + end) / 2);
-		// If element is present at mid, return True
-		if (arr[mid][0] === x[0]) return mid;
-		// Else look in left or right half accordingly
-		else if (arr[mid][0] < x[0])
-			start = mid + 1;
-		else
-			end = mid - 1;
-	}
-	return -1;
-}
-
-//Utility function to add an element to a sorted array.
-//O(n)
-function add_To_Sorted_Array(element, arr) {
-	if (arr.length == 0) {
-		arr.push(element);
-		return;
-	}
-
-	//Avoid Duplicate Latitude longitude co-ordinates
-	let index = BinarySearch2dArray(element[0], arr);
-	if (index != -1 && (arr[index][1] == element[1])) {
-		return;
-	}
-	arr.push(element);
-
-	for (let i = arr.length - 1; i > 0; i--) {
-		let temp2 = (arr[i])[0];
-		if (((arr[i])[0]) < ((arr[i - 1])[0])) {
-			let temp = arr[i];
-			arr[i] = arr[i - 1];
-			arr[i - 1] = temp;
-		} else {
-			break;
-		}
-	}
-	return;
-}
-
-//Create a 2d locations Array to store previous LatLng.
-const locations = [];
-
 //Create Map from Mapbox and LeafJs
 L.mapbox.accessToken = 'pk.eyJ1Ijoic3JpamFuMTIxNCIsImEiOiJjazMyazBkbDAwZGIxM21sYjF6NnVqbnAxIn0.jtPTRywpGF6mJ2ZRbtWJmw';
 const mymap = L.map('map_id').setView([40, -100], 3);
@@ -59,23 +8,17 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 	accessToken: 'pk.eyJ1Ijoic3JpamFuMTIxNCIsImEiOiJjazMyazBkbDAwZGIxM21sYjF6NnVqbnAxIn0.jtPTRywpGF6mJ2ZRbtWJmw'
 }).addTo(mymap);
 
-///handle when user presses the "ENTER" key on text box.
-$("#input_id").on("keydown",function(event){
-	// Number 13 is the "Enter" key on the keyboard
-	if (event.keyCode === 13) {
-	  // Cancel the default action, if needed
-	  event.preventDefault();
-	  // Trigger the button element with a click
-	  $("#button_id").click();
-	}
-})
+//Create a 2d locations Array to store previous LatLng.
+const locations = [];
+
 
 
 ///The form button click handler.
+///The most important part of this script.
+///Takes in input from the text box and sends it to the mapbox API to produce the Geo-Cordinates.
 $("#button_id").on('click', function () {
 	//Make the err bar hidden if it is already visible.
 	$("#err_msg_id").css("visibility", "hidden");
-
 	const addMarkerAndPopup=(latlng)=>{
 		//Add Marker to the location and fly there.
 		//Also add pop up to the marker.
@@ -85,7 +28,11 @@ $("#button_id").on('click', function () {
 
 			if(BinarySearch2dArray(latlng,locations)==-1) {	//Only add a new marker if a marker doesn't exists
 				marker=marker.addTo(mymap);
-				marker.bindPopup("<p class='emphasis'>"+($("#input_id").val()).toUpperCase()+"</p>");	//add user input text to pop up.
+				//Bind popup to the marker.
+				let p=$('<p></p>');
+				p.append(($("#input_id").val()).toUpperCase());
+				p.attr('class','emphasis');
+				marker.bindPopup(p[0]);	//add user input text to pop up.
 			}
 			mymap.flyTo(latlng, 8);
 		}
@@ -98,27 +45,36 @@ $("#button_id").on('click', function () {
 			return console.error('error');
 		}
 		if (data.latlng) {
-			$("#records_id").append("<p class='data_id italics' >" +
-				$("#input_id").val().toUpperCase() +
-				":  " + data.latlng
-				+ "</p>");
+			//Create paragraph element with lat & lng attributes.
+			let p=$('<p></p>');
+			p.append($("#input_id").val().toUpperCase() + ":  " + data.latlng);
+			p.attr('class','data_class italics');
+			p.attr('lat',data.latlng[0]);
+			p.attr('lng',data.latlng[1]);
+			$("#records_id").append(p);
 
+			//Add click handler to every new paragraph added.
+			p.click(function(event){
+				mymap.flyTo([$(this).attr('lat'),$(this).attr('lng')], 8);
+			});			
 			addMarkerAndPopup(data.latlng);
-
 			add_To_Sorted_Array(data.latlng, locations);	//List of all marked locations
-
 		} else {
 			//Make Error Message visible
 			$("#err_msg_id").css("visibility", "visible");
 		}
 	}
-
 	//geocode the user input location
 	const geocoder = L.mapbox.geocoder('mapbox.places');
 	geocoder.query($("#input_id").val(), add_Location_To_Side_bar);
 });
 
-///------The code below adds a dragability to the Box in the Map------
+
+
+
+
+
+///The code below adds a dragability to the Box in the Map
 dragElement($("#text_area_id").get(0));
 function dragElement(elmnt) {
 	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -158,3 +114,58 @@ function dragElement(elmnt) {
 	  document.onmousemove = null;
 	}
 }
+
+//Utility function to binary search the first array cloumn of a 2d array sorted according to the first column.
+//O(Log(n))
+function BinarySearch2dArray(x, arr) {
+	let start = 0, end = arr.length - 1;
+	// Iterate while start not meets end
+	while (start <= end) {
+		// Find the mid index
+		let mid = Math.floor((start + end) / 2);
+		// If element is present at mid, return True
+		if (arr[mid][0] === x[0]) return mid;
+		// Else look in left or right half accordingly
+		else if (arr[mid][0] < x[0])
+			start = mid + 1;
+		else
+			end = mid - 1;
+	}
+	return -1;
+}
+//Utility function to add an element to a sorted array.
+//O(n)
+function add_To_Sorted_Array(element, arr) {
+	if (arr.length == 0) {
+		arr.push(element);
+		return;
+	}
+	//Avoid Duplicate Latitude longitude co-ordinates
+	let index = BinarySearch2dArray(element[0], arr);
+	if (index != -1 && (arr[index][1] == element[1])) {
+		return;
+	}
+	arr.push(element);
+	for (let i = arr.length - 1; i > 0; i--) {
+		let temp2 = (arr[i])[0];
+		if (((arr[i])[0]) < ((arr[i - 1])[0])) {
+			let temp = arr[i];
+			arr[i] = arr[i - 1];
+			arr[i - 1] = temp;
+		} else {
+			break;
+		}
+	}
+	return;
+}
+
+///handle when user presses the "ENTER" key on text box.
+$("#input_id").on("keydown",function(event){
+	// Number 13 is the "Enter" key on the keyboard
+	if (event.keyCode === 13) {
+	  // Cancel the default action, if needed
+	  event.preventDefault();
+	  // Trigger the button element with a click
+	  $("#button_id").click();
+	}
+})
